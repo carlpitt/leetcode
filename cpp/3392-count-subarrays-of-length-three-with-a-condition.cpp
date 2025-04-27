7e75e7f964cf2f844f63c33876aae5e3ee3969b4
Carl Pittenger <carlpittenger@protonmail.com>
2025-04-27 08:43:10 -0400

    add 3392

---
+++ cpp/3392-count-subarrays-of-length-three-with-a-condition.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+
+using uint = unsigned;
+
+class Solution {
+public:
+    [[nodiscard]] constexpr auto
+    countSubarrays(const std::vector<int>& nums) const noexcept -> uint {
+        auto ans = 0U;
+
+        for (auto i = 0U; i < nums.size() - 2; ++i) {
+            if (2 * (nums[i] + nums[i + 2]) == nums[i + 1]) {
+                ++ans;
+            }
+        }
+
+        return ans;
+    }
+};
+
