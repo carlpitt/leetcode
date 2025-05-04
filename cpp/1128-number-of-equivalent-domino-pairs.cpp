15deb9373c7cef3ccfa6c35d86a8d1b0ea31a632
Carl Pittenger <carlpittenger@protonmail.com>
2025-05-04 01:19:38 -0400

    add 1128

---
+++ cpp/1128-number-of-equivalent-domino-pairs.cpp
@@ -0,0 +1,25 @@
+#include <array>
+#include <vector>
+
+using uint = unsigned;
+
+class Solution {
+    public:
+        static constexpr auto numEquivDominoPairs =
+            [] [[nodiscard]] (const std::vector<std::vector<int>> &dominoes
+            ) noexcept -> uint {
+            auto freq = std::array<uint, 100>{};
+            auto ans = 0U;
+
+            for (const auto &domino : dominoes) {
+                const auto x = static_cast<uint>(
+                    10 * std::min(domino[0], domino[1]) +
+                    std::max(domino[0], domino[1])
+                );
+                ans += freq[x];
+                ++freq[x];
+            }
+
+            return ans;
+        };
+};
